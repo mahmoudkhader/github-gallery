301ee3ecd3acd97f10096fdf291b7474d74a6bdc
Mahmoud Khader <mahmoudakhader@gmail.com>
2020-04-27 12:33:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+  // initialize the repos state with an empty array
   const [repos, setRepos] = useState([]);
 
+  // once the component is mounted, fetch the repos data from the github API
   useEffect(() => {
     fetch(`https://api.github.com/users/mahmoudkhader/repos?`)
       // this gives us a promise back that we need to map to json
       .then((res) => res.json())
-      // this gives us data, which will set the state, where we set the repos to the data coming in
+      // once the promise is resolved, set the repos state to the json data sent from the API
       .then((data) => {
         setRepos(data);
       })
+      // catch and log errors
       .catch((err) => console.log(err));
   }, []);
 
   // create repoItems variables and map the repos
   const repoItems = repos.map((repo) => (
-    // set that to a div, with the key of the repo id (each repo will be diesplayed in teh div)
+    // return a col containing a card, and set each repo item's key to it's respective id property(given by the API)
     <div key={repo.id} className="col-xl-4 col-md-6 mb-3">
       <div className="card text-white bg-primary h-100">
         <div className="card-body">
@@ -54,6 +57,7 @@ const App = () => {
     </div>
   ));
 
+  // render the page
   return (
     <div className="container py-5">
       <div className="row mb-5">
@@ -95,6 +99,7 @@ const App = () => {
           <h2>some of my work...</h2>
         </div>
       </div>
+      {/* Render the repoItems object created above */}
       <div className="row">{repoItems}</div>
     </div>
   );
